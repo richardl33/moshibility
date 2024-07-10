6bab2991c21c209b00ae8c264cb427ab7735c7c9
Richard Lee <richalee@microsoft.com>
2024-07-09 21:23:50 -0400

    touchables

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, Image, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   const handlPress = () => console.log("Text pressed");
@@ -8,13 +15,15 @@ export default function App() {
       <Text numberOfLines={1} onPress={handlPress}>
         Hello World!
       </Text>
-      <Image
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/seed/picsum/200/300",
-        }}
-      />
+      <TouchableOpacity onPress={() => console.log("Image Tapped")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/seed/picsum/200/300",
+          }}
+        />
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
